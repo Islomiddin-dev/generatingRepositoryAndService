102d520daf4f2f280096620279363b6692dd390c
Islomiddin <hamisha0327@gmail.com>
2023-03-11 10:22:09 +0500

    fix-directory

--- src/Console/Commands/MakeRepository.php
+++ src/Console/Commands/MakeRepository.php
@@ -9,32 +9,30 @@ class MakeRepository extends Command
     protected $signature = 'make:repository {name}';
     protected $description = 'Create a new repository';
 
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
     public function handle()
     {
         $name = $this->argument('name');
-        $stubInterface = file_get_contents(__DIR__ . '/../../Stubs/RepositoryInterface.stub');
-        $stubInterface = str_replace('Model', $name, $stubInterface);
 
-        $stub = file_get_contents(__DIR__ . '/../../Stubs/Repository.stub');
-        $stub = str_replace('Model', $name, $stub);
+        $stubInterface = file_get_contents(__DIR__ . '/../../Stubs/ModelRepositoryInterface.stub');
+        $stubInterface = str_replace('Model', $name, $stubInterface);
+        $interfacePath = app_path("Interfaces/Repositories/{$name}RepositoryInterface.php");
 
         if (!file_exists(app_path('Interfaces/Repositories'))) {
             mkdir(app_path('Interfaces/Repositories'), 0777, true);
         }
 
-        file_put_contents(app_path("Interfaces/Repositories/{$name}RepositoryInterface.php"), $stubInterface);
-        $this->info("[Interfaces/Repositories/{$name}RepositoryInterface.php] repository interface created successfully");
+        file_put_contents($interfacePath, $stubInterface);
+        $this->info("[{$interfacePath}] repository interface created successfully");
+
+        $stub = file_get_contents(__DIR__ . '/../../Stubs/ModelRepository.stub');
+        $stub = str_replace('Model', $name, $stub);
+        $repositoryPath = app_path("Repositories/{$name}Repository.php");
 
         if (!file_exists(app_path('Repositories'))) {
             mkdir(app_path('Repositories'), 0777, true);
         }
 
-        file_put_contents(app_path("Repositories/{$name}Repository.php"), $stub);
-        $this->info("[Repositories/{$name}Repository.php] repository created successfully");
+        file_put_contents($repositoryPath, $stub);
+        $this->info("[{$repositoryPath}] repository created successfully");
     }
 }
--- src/Console/Commands/MakeService.php
+++ src/Console/Commands/MakeService.php
@@ -12,25 +12,28 @@ class MakeService extends Command
     public function handle()
     {
         $name = $this->argument('name');
+
         $stubInterface = file_get_contents(__DIR__ . '/../../Stubs/ModelServiceInterface.stub');
         $stubInterface = str_replace('Model', $name, $stubInterface);
+        $interfacePath = app_path("Interfaces/Services/{$name}ServiceInterface.php");
+
+        if (!file_exists(app_path('Interfaces/Services'))) {
+            mkdir(app_path('Interfaces/Services'), 0777, true);
+        }
+
+        file_put_contents($interfacePath, $stubInterface);
+        $this->info("[{$interfacePath}] service interface created successfully");
 
         $stub = file_get_contents(__DIR__ . '/../../Stubs/ModelService.stub');
         $stub = str_replace('Model', $name, $stub);
         $stub = str_replace('model', strtolower($name), $stub);
+        $repositoryPath = app_path("Services/{$name}Service.php");
 
-        if (!file_exists(app_path("Interfaces/Services"))) {
-            mkdir(app_path("Interfaces/Services"), 0777, true);
+        if (!file_exists(app_path('Services'))) {
+            mkdir(app_path('Services'), 0777, true);
         }
 
-        file_put_contents(app_path("Interfaces/Services/{$name}ServiceInterface.php"), $stubInterface);
-        $this->info("[Interfaces/Services/{$name}ServiceInterface.php] service interface created successfully");
-
-        if (!file_exists(app_path("Services"))) {
-            mkdir(app_path("Services"), 0777, true);
-        }
-
-        file_put_contents(app_path("Services/{$name}Service.php"), $stub);
-        $this->info("[Services/{$name}Service.php] service created successfully");
+        file_put_contents($repositoryPath, $stub);
+        $this->info("[{$repositoryPath}] service created successfully");
     }
 }
